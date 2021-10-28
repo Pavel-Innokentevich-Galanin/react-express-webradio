8acc2fb5f80793722ef4e3b806be6b267ea9f2d0
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2021-10-28 22:20:18 +0300

    Galanin_Pavel : Added func play/pause audio

--- src/frontend/src/components/App/App.jsx
+++ src/frontend/src/components/App/App.jsx
@@ -3,9 +3,11 @@ import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
     faPlay,
+    faPause,
     faArrowRight,
     faArrowLeft,
 } from '@fortawesome/free-solid-svg-icons';
+import { Howl } from "howler";
 
 import "./App.css";
 import styles from "./App.module.css";
@@ -17,8 +19,13 @@ export default function App() {
     const [RadioName, SetRadioName] = useState("Radio name");
     const [RadioSrcImage, SetSrcImage] = useState("");
     const [RadioDescription, SetRadioDescription] = useState("Description");
+    const [RadioAudio, SetRadioAudio] = useState("");
+    //Play
+    const [RadioHowler, SetRadioHowler] = useState(new Howl({ src: [RadioAudio], html5: true, }));
+    const [RadioIsPlay, SetRadioIsPlay] = useState(false);
+    const [RadioPlayIcon, SetRadioPlayIcon] = useState(faPlay);
 
-    (function() { // Constructor
+    (function () { // Constructor
         if (ConstructorIsRun) {
             return;
         }
@@ -29,22 +36,30 @@ export default function App() {
     function get_json() {
         const URL = `${process.env.REACT_APP__API_URL}:${process.env.REACT_APP__API_PORT}/gpi-get-radio`;
         axios.get(URL)
-        .then(function(response) {
-            let arr = response.data;
-            console.log(arr);
-            
-            if (arr[0]) {
-                console.log(arr[0]);
-                SetRadioData(arr);
-                SetRadioName(arr[0]["station"]["display_name"]);
-                SetRadioDescription(arr[0]["station"]["description"]);
-                SetSrcImage(arr[0]["station"]["images"]["station"]);
-            }
-        })
-        .catch(function(err) {
-            console.error(err)
-            alert("Not work API sever");
-        });
+            .then(function (response) {
+                let arr = response.data;
+                console.log(arr);
+
+                if (arr[0]) {
+                    console.log(arr[0]);
+                    SetRadioData(arr);
+                    SetRadioName(arr[0]["station"]["display_name"]);
+                    SetRadioDescription(arr[0]["station"]["description"]);
+                    SetSrcImage(arr[0]["station"]["images"]["station"]);
+                    SetRadioAudio(arr[0]["station"]["stream_url"]);
+                    const hwr = new Howl({
+                        src: [
+                            arr[0]["station"]["stream_url"]
+                        ],
+                        html5: true,
+                    });
+                    SetRadioHowler(hwr);
+                }
+            })
+            .catch(function (err) {
+                console.error(err)
+                alert("Not work API sever");
+            });
     }
 
     function gpi_prev_song() {
@@ -59,6 +74,18 @@ export default function App() {
         SetRadioName(RadioData[index]["station"]["display_name"]);
         SetRadioDescription(RadioData[index]["station"]["description"]);
         SetSrcImage(RadioData[index]["station"]["images"]["station"]);
+        SetRadioAudio(RadioData[index]["station"]["stream_url"]);
+
+        RadioHowler.stop();
+        SetRadioIsPlay(false);
+        SetRadioPlayIcon(faPause);
+        const hwr = new Howl({
+            src: [
+                RadioData[index]["station"]["stream_url"]
+            ],
+            html5: true,
+        });
+        SetRadioHowler(hwr);
     }
 
     function gpi_next_song() {
@@ -73,6 +100,32 @@ export default function App() {
         SetRadioName(RadioData[index]["station"]["display_name"]);
         SetRadioDescription(RadioData[index]["station"]["description"]);
         SetSrcImage(RadioData[index]["station"]["images"]["station"]);
+        SetRadioAudio(RadioData[index]["station"]["stream_url"]);
+
+        RadioHowler.stop();
+        SetRadioIsPlay(false);
+        SetRadioPlayIcon(faPause);
+        const hwr = new Howl({
+            src: [
+                RadioData[index]["station"]["stream_url"]
+            ],
+            html5: true,
+        });
+        SetRadioHowler(undefined);
+        SetRadioHowler(hwr);
+    }
+
+    function gpi_play() {
+        if (RadioIsPlay) {
+            SetRadioIsPlay(false);
+            SetRadioPlayIcon(faPause);
+            RadioHowler.pause();
+        }
+        else {
+            SetRadioIsPlay(true);
+            SetRadioPlayIcon(faPlay);
+            RadioHowler.play();
+        }
     }
 
     return (
@@ -101,14 +154,12 @@ export default function App() {
                 >
                     <FontAwesomeIcon icon={faArrowLeft} />
                 </button>
-                {/*
                 <button
                     className={styles.player__button}
-                    onClick={event => get_json()}
+                    onClick={event => gpi_play()}
                 >
-                    <FontAwesomeIcon icon={faPlay} />
+                    <FontAwesomeIcon icon={RadioPlayIcon} />
                 </button>
-                */}
                 <button
                     className={styles.player__button}
                     onClick={event => gpi_next_song()}
