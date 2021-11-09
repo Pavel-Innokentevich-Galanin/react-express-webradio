a4eea120c668fe7303f1e0bf53b7641efb55cd54
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2021-11-09 16:59:29 +0300

    Galanin_Pavel : Updtd frontend - add audiospectrum

--- src/frontend/src/components/App/App.jsx
+++ src/frontend/src/components/App/App.jsx
@@ -1,171 +1,59 @@
-import axios from 'axios';
-import {
-    useEffect,
-    useState,
-} from 'react';
+import { useState, useRef } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-    faPlay,
-    faPause,
-    faArrowRight,
-    faArrowLeft,
-} from '@fortawesome/free-solid-svg-icons';
-import { Howl } from "howler";
+import { faPlay, faPause } from '@fortawesome/free-solid-svg-icons';
+import AudioSpectrum from 'react-audio-spectrum'
 
 import "./App.css";
 import styles from "./App.module.css";
 
 export default function App() {
-    const [gpi_radioIndex, gpi_setRadioIndex] = useState(0);
-    const [gpi_radioArray, gpi_setRadioArray] = useState([
-        {
-            station: {
-                images: {
-                    station: "",
-                },
-                stream_url: "",
-                name: "Name",
-                description: "Description...",
-            },
-        }
-    ]);
-    //Play
-    const [gpi_radioHowler, gpi_setRadioHowler] = useState();
     const [gpi_radioIsPlay, gpi_setRadioIsPlay] = useState(false);
-    const [gpi_radioPlayIcon, gpi_setRadioPlayIcon] = useState(faPlay);
+    const gpi_audio_ref = useRef(null);
 
-    useEffect(() => {
-        get_json();
-    }, []);
-
-    async function get_json() {
-        const GPI_URL = `${process.env.REACT_APP__API_URL}:${process.env.REACT_APP__API_PORT}/gpi-get-radio`;
-        try {
-            const GPI_RESPONSE = await axios.get(GPI_URL)
-            const GPI_ARR = GPI_RESPONSE.data;
-            console.log(GPI_ARR);
-    
-            if (GPI_ARR[0]) {
-                console.log(GPI_ARR[0]);
-                gpi_setRadioArray(GPI_ARR);
-                const GPI_HWR = new Howl({
-                    src: [
-                        GPI_ARR[0].station.stream_url
-                    ],
-                    html5: true,
-                });
-                gpi_setRadioHowler(GPI_HWR);
-            }
-        }
-        catch (gpi_err) {
-            console.error(gpi_err);
-            alert("Not work API sever");
-        }
-    }
-
-    function gpi_prev_song() {
-        let gpi_index = gpi_radioIndex;
-        gpi_index -= 1;
-
-        if (gpi_radioArray[gpi_index] === undefined) {
-            alert("It is the first station");
-            return;
-        }
-
-        gpi_setRadioIndex(gpi_index);
-        console.log(gpi_radioArray[gpi_index]);
-
-        gpi_radioHowler.stop();
-        gpi_setRadioIsPlay(false);
-        gpi_setRadioPlayIcon(faPlay);
-        const GPI_HWR = new Howl({
-            src: [
-                gpi_radioArray[gpi_index].station.stream_url,
-            ],
-            html5: true,
-        });
-        gpi_setRadioHowler(GPI_HWR);
-    }
-
-    function gpi_next_song() {
-        let gpi_index = gpi_radioIndex;
-        gpi_index += 1;
-
-        if (gpi_radioArray[gpi_index] === undefined) {
-            alert("This is the last station");
-            return;
-        }
-
-        gpi_setRadioIndex(gpi_index);
-        console.log(gpi_radioArray[gpi_index]);
-
-        gpi_radioHowler.stop();
-        gpi_setRadioIsPlay(false);
-        gpi_setRadioPlayIcon(faPlay);
-        const GPI_HWR = new Howl({
-            src: [
-                gpi_radioArray[gpi_index].station.stream_url,
-            ],
-            html5: true,
-        });
-        gpi_setRadioHowler(GPI_HWR);
-    }
-
-    function gpi_play() {
-        if (gpi_radioHowler === undefined) {
-            alert("Audio not loaded");
-            return;
-        }
-
+    function gpi_play_pause() {
         if (gpi_radioIsPlay) {
+            document.getElementById(gpi_audio_ref.current.id).pause();
             gpi_setRadioIsPlay(false);
-            gpi_setRadioPlayIcon(faPlay);
-            gpi_radioHowler.pause();
+            return;
         }
-        else {
-            gpi_setRadioIsPlay(true);
-            gpi_setRadioPlayIcon(faPause);
-            gpi_radioHowler.play();
-        }
+        document.getElementById(gpi_audio_ref.current.id).play();
+        gpi_setRadioIsPlay(true);
     }
 
     return (
-        <div className={styles.b_player}>
-            <div className={styles.player__index}>
-                {gpi_radioIndex}
-            </div>
-            <div className={styles.player_img}>
-                <img
-                    src={gpi_radioArray[gpi_radioIndex].station.images.station}
-                    alt=""
+        <div className={styles.player}>
+            <div className={styles.player__audio}>
+                <audio
+                    id="gpi-audio"
+                    crossorigin="anonymous"
+                    ref={gpi_audio_ref}
+                    preload="true"
+                    // src={`${process.env.REACT_APP__API_URL}:${process.env.REACT_APP__API_PORT}/gpi-get`}
+                    src={`https://skafari.stream.laut.fm/skafari`}
+                >
+                </audio>
+                <AudioSpectrum
+                    height={200}
+                    width={300}
+                    audioId={'gpi-audio'}
+                    capColor={'red'}
+                    capHeight={2}
+                    meterWidth={2}
+                    meterCount={512}
+                    meterColor={[
+                        { stop: 0, color: 'pink' },
+                        { stop: 1, color: 'red' }
+                    ]}
+                    gap={1}
                 />
             </div>
-            <div className={styles.player_info}>
-                <div className={styles.player_name}>
-                    {gpi_radioArray[gpi_radioIndex].station.name}
-                </div>
-                <div className={styles.player__description}>
-                    {gpi_radioArray[gpi_radioIndex].station.description}
-                </div>
-            </div>
-            <div>
-                <button
-                    className={styles.player__button}
-                    onClick={event => gpi_prev_song()}
-                >
-                    <FontAwesomeIcon icon={faArrowLeft} />
-                </button>
-                <button
-                    className={styles.player__button}
-                    onClick={event => gpi_play()}
-                >
-                    <FontAwesomeIcon icon={gpi_radioPlayIcon} />
-                </button>
-                <button
-                    className={styles.player__button}
-                    onClick={event => gpi_next_song()}
-                >
-                    <FontAwesomeIcon icon={faArrowRight} />
+            <div className={styles.player__b_button}>
+                <button className={styles.player__button} onClick={gpi_play_pause}>
+                    {
+                        gpi_radioIsPlay ?
+                        (<FontAwesomeIcon icon={faPlay} />) :
+                        (<FontAwesomeIcon icon={faPause} />)
+                    }
                 </button>
             </div>
         </div>
