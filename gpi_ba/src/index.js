1bbf3a95956dfb2caa7b57b10009ad41237d2edb
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2022-01-03 14:43:36 +0300

    dev/gpi_ : Updated module ba

--- gpi_ba/src/index.js
+++ gpi_ba/src/index.js
@@ -1,12 +1,22 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+// gpi_ Библиотеки
+const gpi_dotenv = require('dotenv');
+const gpi_express = require('express');
+const gpi_cors = require('cors');
+const gpi_route_radio = require('./route/gpi_route_radio');
 
-const app = express();
-app.use(cors());
-app.listen(process.env.gpi_nodejs_port);
+// gpi_ Инициализация
+gpi_dotenv.config();
+const gpi_app = gpi_express();
+gpi_app.use(gpi_cors());
+
+// gpi_ Запуск сервера
+gpi_app.listen(process.env.gpi_nodejs_port);
 console.log(`Open ${process.env.gpi_nodejs_baseurl}:${process.env.gpi_nodejs_port}/`);
 
-app.use("/", require("./gpi_home"));
-app.use("/gpi_", require("./gpi_home"));
-app.use("/gpi_", require("./gpi_get-json-stream"));
+// gpi_ Роутер home
+gpi_app.get("/", function (gpi_request, gpi_response) {
+    gpi_response.send("Hello, World!");
+})
+
+// gpi_ Роутер radio
+gpi_app.use("/", gpi_route_radio);
---
+++ gpi_ba/src/route/gpi_route_radio.js
@@ -0,0 +1,38 @@
+// gpi_ Библиотеки
+const gpi_express = require('express');
+const gpi_Lautfm = require('lautfm');
+
+// gpi_ Инициализация
+const gpi_router = gpi_express.Router();
+
+// gpi_ Роутер gpi_get_radio
+gpi_router.get("/gpi_get_radio", async function (gpi_request, gpi_resonse) {
+    try {
+        const gpi_laut = new gpi_Lautfm();
+        const gpi_res = await gpi_laut.searchStations({ query: 'ska' });
+       
+        let gpi_result = []; // gpi_ Массив для станций
+        const gpi_arr = gpi_res.results; // gpi_ переходим  в ветку results
+
+        // gpi_ проходимся по массиву станций
+        for (let gpi_i = 0; gpi_i < gpi_arr.length; ++gpi_i) {
+            // gpi_ в массиве станций есть другие станции
+            // gpi_ добавим другие станции в наш массив
+            gpi_result = gpi_result.concat(gpi_arr[gpi_i].items);
+        }
+
+        // gpi_ Теперь у нас есть один массив с разными станциями в gpi_result
+        gpi_resonse.send({
+            "old": gpi_res,
+            "new": gpi_result,
+        });
+    }
+    catch(gpi_error) {
+        gpi_resonse.send({
+            "gpi_code": "500",
+            "gpi_msg": "" + gpi_error,
+        });
+    }
+})
+
+module.exports = gpi_router;
